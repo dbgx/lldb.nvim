ff8ebd3d8d10484fd549a385a620217f93a7f12d
Daniel P. Wright <dani@dpwright.com>
2014-04-08 10:54:42 +0900

    Add recursive expansion of variable children
    This applies to pointers, arrays, structs, etc.  Expand to a depth of 6
    levels (just to avoid recursive definitions exploding out of all
    proportion), indenting each line so that users can make use of vim's
    native folding capabilities if they wish.

--- python-vim-lldb/vim_panes.py
+++ python-vim-lldb/vim_panes.py
@@ -454,14 +454,27 @@ class LocalsPane(FrameKeyValuePane):
     self.show_statics = True
     self.show_in_scope_only = True
 
-  def format_variable(self, var):
-    """ Returns a Tuple of strings "(Type) Name", "Value" for SBValue var """
-    val = var.GetValue()
-    if val is None:
-      # If the value is too big, SBValue.GetValue() returns None; replace with ...
-      val = "..."
+  def format_variable(self, var, indent = 0):
+    """ Returns a list of tuples of strings "(Type) Name", "Value" for SBValue var
+        and its children
+    """
+    MAX_DEPTH = 6
 
-    return ("(%s) %s" % (var.GetTypeName(), var.GetName()), "%s" % val)
+    if indent > MAX_DEPTH:
+      return []
+    else:
+      val = var.GetValue()
+      if val is None:
+        # If the value is too big, SBValue.GetValue() returns None; replace with ...
+        val = "..."
+
+      children = []
+      if var.GetNumChildren() > 0:
+        for x in var:
+          children.extend(self.format_variable(x, indent + 1))
+
+      return [("%s(%s) %s" % (' ' * indent, var.GetTypeName(), var.GetName()),
+              "%s" % val)] + children
 
   def get_frame_content(self, frame):
     """ Returns list of key-value pairs of local variables in frame """
@@ -469,7 +482,12 @@ class LocalsPane(FrameKeyValuePane):
                                    self.show_locals,
                                    self.show_statics,
                                    self.show_in_scope_only)
-    return [self.format_variable(x) for x in vals]
+
+    out = []
+    for v in [self.format_variable(x) for x in vals]:
+      out.extend(v)
+
+    return out
 
 class RegistersPane(FrameKeyValuePane):
   """ Pane that displays the contents of registers """
